cecd8b4fbdcbf5105910b006504d961f98d28deb
Chun Ho Ling <ch.ling@bbpos.com>
2021-07-22 17:54:55 +0800

    Update Open API settings and folder structure.

--- src/main/java/com/bbpos/tms2/services/sample/v1/controllers/SampleController.java
+++ src/main/java/com/bbpos/tms2/services/sample/v1/controllers/SampleController.java
@@ -1,12 +1,27 @@
 package com.bbpos.tms2.services.sample.v1.controllers;
 
-import org.springframework.stereotype.Controller;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/v1/sample")
+@Tag(name="Sample", description = "Sample API")
 public class SampleController {
     @GetMapping()
-    public String get(String value) {
+    @Operation(summary = "Get response from API.", description = "Description here.", tags={ "Sample" })
+    @ApiResponses(
+        @ApiResponse(responseCode = "200", description = "Success.")
+    )
+    public String get(
+        @Parameter(description = "Input value")
+        String value
+    ) {
         return String.format("Server response: %s", value);
     }
 }
--- src/test/java/com/bbpos/tms2/ApplicationTests.java
+++ src/test/java/com/bbpos/tms2/ApplicationTests.java
@@ -2,9 +2,12 @@ package com.bbpos.tms2;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import({BuildProperties.class})
 class ApplicationTests {
 
 	@Test()
