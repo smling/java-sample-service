f35583be134557eb4b8acc82205dd4d3d97bdccf
Chun Ho Ling <ch.ling@bbpos.com>
2021-07-29 16:51:10 +0800

    Add shutdown handling.

---
+++ src/main/java/com/bbpos/tms2/services/sample/configuration/ApplicationConfiguration.java
@@ -0,0 +1,12 @@
+package com.bbpos.tms2.services.sample.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfiguration {
+    @Bean
+    public TerminationBean getTerminateBean() {
+        return new TerminationBean();
+    }
+}
---
+++ src/main/java/com/bbpos/tms2/services/sample/configuration/TerminationBean.java
@@ -0,0 +1,17 @@
+package com.bbpos.tms2.services.sample.configuration;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PreDestroy;
+
+@Component
+public class TerminationBean {
+    private final Logger logger = LogManager.getLogger(TerminationBean.class);
+
+    @PreDestroy
+    public void onDestroy() {
+        logger.info("Service shutdown.");
+    }
+}
