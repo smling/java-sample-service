c084b13604866ef23e01499ad143cf650a8286a4
Chun Ho Ling <ch.ling@bbpos.com>
2021-08-26 16:30:39 +0800

    update publish pipeline.

--- src/main/java/com/bbpos/tms2/services/sample/configuration/ApplicationConfiguration.java
+++ src/main/java/com/bbpos/tms2/services/sample/configuration/ApplicationConfiguration.java
@@ -2,17 +2,11 @@ package com.bbpos.tms2.services.sample.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 
 @Configuration
 public class ApplicationConfiguration {
     @Bean
     public TerminationBean getTerminateBean() {
         return new TerminationBean();
-    }
-
-    @Bean
-    public MethodValidationPostProcessor methodValidationPostProcessor() {
-        return new MethodValidationPostProcessor();
     }
 }
